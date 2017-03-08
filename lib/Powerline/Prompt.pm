d65ce75b312ee742a2af6b40d1bf2b1ed7ecd2e8
Jarkko Haapalainen <jarkko@viidakko.fi>
2017-03-08 18:52:11 +0200

    added path segment

--- lib/Powerline/Prompt.pm
+++ lib/Powerline/Prompt.pm
@@ -3,6 +3,7 @@ use v6;
 use Powerline::Prompt::Segment;
 use Powerline::Prompt::Segment::Git;
 use Powerline::Prompt::Segment::Root;
+use Powerline::Prompt::Segment::Path;
 
 role Powerline::Prompt {
 
@@ -11,7 +12,6 @@ role Powerline::Prompt {
     has Str $.root;
     has Str $.path = '.';
     has Int $.exit = 0;
-    has Str $.separator = '';
 
     method draw {
         my @segments;
@@ -21,6 +21,9 @@ role Powerline::Prompt {
 
         # Hostname
         @segments.push( Powerline::Prompt::Segment.new(text => $.host, foreground => 250, background => 238) );
+
+        # Path
+        @segments.push( Powerline::Prompt::Segment::Path.new(homedir => %*ENV<HOME>, cwd => $.path) );
 
         # Git
         @segments.push( Powerline::Prompt::Segment::Git.new(cwd => $.path) );
@@ -38,13 +41,7 @@ role Powerline::Prompt {
                 $next = @segments[0];
             }
 
-            $prompt ~= sprintf(
-                '\[\e[38;5;' ~ $segment.foreground ~ 'm\]\[\e[48;5;' ~ $segment.background ~ 'm\]' ~
-                $segment.text ~
-                ( $next
-                    ?? '\[\e[38;5;' ~ $segment.background ~ 'm\]\[\e[48;5;' ~ $next.background ~ 'm\]'
-                    !! '\[\e[0m\]' ~ '\[\e[38;5;' ~ $segment.background ~ 'm\]'
-                ) ~ $.separator ~ '\[\e[0m\]');
+            $prompt ~= $segment.draw($next);
         }
 
         $prompt;
